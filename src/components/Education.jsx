aa0176d20f9f49b4e91b7c84e84a170754e9560f
Subhajit Naskar <133232117+subhajit-gitch@users.noreply.github.com>
2024-08-03 23:19:00 +0530

    education card fix

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -20,29 +20,29 @@ const educationData = [
     {
         yearRange: '2021 - 2023',
         degree: 'Post Graduation - MCA',
-        institution: 'College: Techno College Hooghly, University: MAKAUT',
-        details: '',
+        institution: 'College: Techno College Hooghly',
+        details: 'University: M.A.K.A.U.T',
         icon: <FaGraduationCap />
     },
     {
         yearRange: '2017 - 2020',
         degree: 'Graduation - BCA',
-        institution: 'College: Techno India Hooghly Campus, University: MAKAUT',
-        details: '',
+        institution: 'College: Techno India Hooghly Campus',
+        details: 'University: M.A.K.A.U.T',
         icon: <FaGraduationCap />
     },
     {
         yearRange: '2017',
         degree: 'Higher Secondary - 12th (Intermediate)',
-        institution: 'School: Serampore Union Institution, Board: WBCHSE',
-        details: '',
+        institution: 'School: Serampore Union Institution',
+        details: 'Board: W.B.C.H.S.E',
         icon: <FaGraduationCap />
     },
     {
         yearRange: '2015',
         degree: 'Secondary - 10th (Matriculation)',
-        institution: 'School: Serampore Union Institution, Board: WBBSE',
-        details: '',
+        institution: 'School: Serampore Union Institution',
+        details: 'Board: W.B.B.S.E',
         icon: <FaGraduationCap />
     }
 ];
